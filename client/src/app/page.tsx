1cda959d97c736d9ab01add1ba334cd20db455c6
Oscar Ariel Saucedo <arielsaucedo.dev@gmail.com>
2025-04-05 00:14:31 -0300

    Setup inicial del proyecto

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from '@/components/ui';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold'>Invera Frontend Challenge</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
