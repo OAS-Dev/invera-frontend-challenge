5d77348f8119ee69d9ad8e86551e8512c188cb2b
Oscar Ariel Saucedo <arielsaucedo.dev@gmail.com>
2025-04-06 17:06:20 -0300

    Se corrigen estilos

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,16 @@
-import {Header, Statistics} from '@/components';
+import {DashboardInfo, Header, Statistics} from '@/components';
 // import {ToggleThemeButton} from '@/components/shared';
 
 export default function HomePage() {
   return (
-    <div className='flex flex-col gap-8 mx-4 mt-8 lg:mx-10'>
+    <div className='flex flex-col gap-8 mx-4 mt-8 md:m-14 lg:mx-10'>
       {/* <div className='flex justify-end mt-4'>
         <ToggleThemeButton />
       </div> */}
 
       <Header />
+
+      <DashboardInfo />
 
       <Statistics />
     </div>
