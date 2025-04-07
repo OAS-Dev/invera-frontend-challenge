153e52d86bde0cc9265cdb3c23e81afde67c7cbf
Oscar Ariel Saucedo <arielsaucedo.dev@gmail.com>
2025-04-07 17:16:24 -0300

    Se activa boton para cambio modo oscuro/claro

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,12 @@
 import {DashboardInfo, Header, Statistics, UsersData} from '@/components';
-// import {ToggleThemeButton} from '@/components/shared';
+import {ToggleThemeButton} from '@/components/shared';
 
 export default function HomePage() {
   return (
     <div className='flex flex-col gap-8 mx-4 my-8 md:m-12 lg:mx-10'>
-      {/* <div className='flex justify-end mt-4'>
+      <div className='flex justify-end mt-4'>
         <ToggleThemeButton />
-      </div> */}
+      </div>
 
       <Header />
 
